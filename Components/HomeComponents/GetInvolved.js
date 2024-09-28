2e2d7b13f917063af2a31d017f07fd8fa93532fe
sibiraj-002 <shansibi2002@gmail.com>
2024-09-28 10:05:29 +0530

    get involved updated

--- Components/HomeComponents/GetInvolved.js
+++ Components/HomeComponents/GetInvolved.js
@@ -2,20 +2,21 @@ import React from "react";
 
 const GetInvolved = () => {
   return (
-    <div className="relative w-full h-[50vh] bg-[url(/images/get-involved-bg.png)] bg-no-repeat bg-cover bg-center">
+    <div className="relative w-full h-full bg-[url(/images/get-involved-bg.png)] bg-no-repeat bg-cover bg-center">
       {/* Overlay */}
       <div className="absolute inset-0"></div>
 
       {/* Content */}
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4">
-        <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold md:-mt-24 ">
+      <div className="relative z-10 flex flex-col items-center justify-start h-full text-center text-white px-4">
+        <h1 className="text-5xl md:text-9xl font-bold absolute md:-top-5">
           Get Involved
         </h1>
-        <p className="mt-4 md:mt-10 max-w-2xl text-sm md:text-lg font-light text-white/70">
+        <div className="py-10 md:pt-28">
+        <p className="py-4 max-w-2xl text-sm md:text-lg font-light text-white/70">
           Whether you&apos;re a social impact organisation, investor, or
           policymaker, your support is critical to building a resilient India.
         </p>
-        <p className="mt-4 max-w-2xl text-sm md:text-lg font-light text-white/70">
+        <p className="py-4 max-w-2xl text-sm md:text-lg font-light text-white/70">
           <span className="font-bold text-white">
             Join the Social Protection Coalition
           </span>{" "}
@@ -23,7 +24,7 @@ const GetInvolved = () => {
           together to transform social security into a force for economic and
           social progress.
         </p>
-        <p className="mt-4 text-sm md:text-lg font-light text-white/70">
+        <p className="py-4 text-sm md:text-lg font-light text-white/70">
           To learn how you can be part of this national effort, contact us at:{""}
           <span className="bg-custom-yellow md:p-2 p-1 text-black mx-2">
             setu.connect@Catalysts.org
@@ -31,13 +32,14 @@ const GetInvolved = () => {
         </p>
 
         {/* Contact Person */}
-        <p className="my-4 text-sm md:text-lg font-light text-white/70">
+        <p className="pt-4 text-sm md:text-lg font-light text-white/70">
           Connect with Priyamvada Tiwari, Associate Director-Platforms & Policy
           at{""}
           <span className="bg-custom-yellow md:p-2 p-1 text-black mx-2">
             priyamvada@Catalysts.org
           </span>
         </p>
+      </div>
       </div>
     </div>
   );
